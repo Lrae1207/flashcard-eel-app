747269817233a5e392e7ace348ab10da91f3508f
Lrae <lraeprogramming@gmail.com>
2025-01-25 13:40:16 -0800

    more progress

--- src/script.py
+++ src/script.py
@@ -4,21 +4,5 @@ import os
 
 cur_dir = os.getcwd()
 
-# returns parsed json object
-@eel.expose
-def read_file(filename):
-    with open("web/sets/" + filename, 'r+') as file:
-        data = json.load(file)
-        print(data)
-        return data
-    return None
-
-# return all file names in web/sets
-@eel.expose
-def get_file_names():
-    os.chdir(cur_dir + "/web/sets")
-    names = os.listdir()
-    return names
-
 eel.init('web')
 eel.start('start.html')
--- src/web/function.js
+++ src/web/function.js
@@ -1,34 +1,34 @@
+setName = null;
+setData = null;
 
-let filenames = null;
-let setSection = document.getElementById('set-section');
+const fileInput = document.getElementById("set-select");
+const errorText = document.getElementById("error-text");
+const previewText = document.getElementById("preview-text");
 
-// wrapper
-async function getFileNames() {
-    return await eel.get_file_names()();
+fileInput.addEventListener("change", onFileChange, false);
+function onFileChange() {
+    fileList = this.files;
+    if (fileList.length < 1) {
+        displayError("file selection failed");
+        return;
+    }
+    var setName = fileList.name;
+    var fileReader = new FileReader();
+    fileReader.onload = function() {
+        text = fileReader.result.slice(0,497) + "...";
+
+        previewText.textContent = text;
+    }
+    fileReader.readAsText(this.files[0]);
 }
 
-// load all of the files json in sets directory
-window.addEventListener('load', async function() {
-    filenames = await getFileNames();
-    filenames.forEach(name => {
-        let childNode = this.document.createElement("p");
-        childNode.innerText = name;
-        let childButton = this.document.createElement("button");
-        childButton.innerText = "Select" + name;
-        childButton.onclick = "loadSetPage(" + name + ");";
-        setSection.appendChild(childNode);
-        setSection.appendChild(childButton);
-    });
-    console.log(filenames);
-});
+function displayError(message) {
+    errorText.innerText = "ERROR: " + message;
+}
 
-async function loadSetPage(setName) {
+function loadSetPage() {
+
     sessionStorage.setItem('setName',setName);
+    sessionStorage.setItem('setData',JSON.stringify(setData));
     document.URL ='set.html'
-}
-
-async function getJSONFromPython(fileName) {
-    let json = await eel.read_file()();
-    console.log(json);
-    return json;
 }
