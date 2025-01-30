6370ea1cf3432f888fd1e67d1d542796e908b450
Lrae <lraeprogramming@gmail.com>
2025-01-29 21:19:30 -0800

    more progress

--- src/web/function.js
+++ src/web/function.js
@@ -5,6 +5,7 @@ const fileInput = document.getElementById("set-select");
 const errorText = document.getElementById("error-text");
 const previewText = document.getElementById("preview-text");
 
+
 fileInput.addEventListener("change", onFileChange, false);
 function onFileChange() {
     fileList = this.files;
@@ -15,14 +16,38 @@ function onFileChange() {
     var setName = fileList.name;
     var fileReader = new FileReader();
     fileReader.onload = function() {
-        text = fileReader.result.slice(0,497) + "...";
-
-        previewText.textContent = text;
+        try {
+            setData = JSON.parse(fileReader.result);
+            
+            displayPreview();
+        } catch {
+            displayError("file was improperly formatted/wasn't json file");
+        }
     }
     fileReader.readAsText(this.files[0]);
 }
 
+previewText.setAttribute('style', 'white-space: pre;');
+const previewCharLimit = 500;
+function displayPreview() {
+    var previewStr = "";
+    var index = 0;
+    while (previewStr.length < previewCharLimit && index < setData.length) {
+        var term = setData[index]['term'];
+        var desc = setData[index]['description'];
+        previewStr += term + ": " + desc + "\n"; 
+        index++;
+    }
+
+    if (previewStr.length > previewCharLimit) {
+        previewStr = previewStr.slice(0,previewCharLimit - 3) + "..."; // truncate
+    }
+
+    previewText.textContent = previewStr;
+}
+
 function displayError(message) {
+    errorText.hidden = false;
     errorText.innerText = "ERROR: " + message;
 }
 
