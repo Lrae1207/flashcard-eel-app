8e3caaf0fe40f55e99e05dc90ad6b0f6adc732ff
Lrae <lraeprogramming@gmail.com>
2025-02-05 21:11:20 -0800

    Some minor changes; app now functional

--- src/web/function.js
+++ src/web/function.js
@@ -1,16 +1,52 @@
-setName = null;
-setData = null;
+// Set data
+var setName = null;
+var setData = null;
+var setSize = 0;
+var validSet = false;
+
+// study section data
+var currentCardIndex = 0;
+var isShuffled = false;
+var isTermShownDefault = true;
+var isTermShown = true;
+
+// divs
+const divSetSelection = document.getElementById("set-section");
+const divSetPreview = document.getElementById("set-preview-section");
+
+const divStudyFlashcard = document.getElementById("study-flashcard-viewport");
+const divStudyCardDisplay = document.getElementById("study-flashcard");
+const divStudyButtons = document.getElementById("study-flashcard-buttons");
+
+const divAbout = document.getElementById('about-section');
+
+const divError = document.getElementById('error-section');
+
+// Misc. elements
+const title = document.getElementById("page-title");
+const navbar = document.getElementById("page-navbar-top");
+const header = document.getElementById("page-header");
 
 const fileInput = document.getElementById("set-select");
-const errorText = document.getElementById("error-text");
-const previewText = document.getElementById("preview-text");
+const previewText = document.getElementById("set-preview-text");
 
+const studyFlashcardCounter = document.getElementById("study-flashcard-counter");
+const studyIndexDisplay = document.getElementById("study-index-display");
+const studyFlashcard = document.getElementById("study-flashcard-display");
+const studyTermDisplay = document.getElementById("study-term-display");
+const studyButtonLeft = document.getElementById("study-button-left");
+const studyButtonRight = document.getElementById("study-button-right");
+const studyButtonShuffle = document.getElementById("study-button-shuffle");
+const studyButtonFlip = document.getElementById("study-button-flip");
+
+const errorText = document.getElementById("error-text");
 
 fileInput.addEventListener("change", onFileChange, false);
 function onFileChange() {
     fileList = this.files;
     if (fileList.length < 1) {
         displayError("file selection failed");
+        validSet = false;
         return;
     }
     var setName = fileList.name;
@@ -18,10 +54,15 @@ function onFileChange() {
     fileReader.onload = function() {
         try {
             setData = JSON.parse(fileReader.result);
-            
+            setSize = setData.length;
+            validSet = true;
             displayPreview();
+            clearError();
         } catch {
+            clearPreview();
             displayError("file was improperly formatted/wasn't json file");
+            validSet = false;
+            setSize = 0;
         }
     }
     fileReader.readAsText(this.files[0]);
@@ -46,14 +87,122 @@ function displayPreview() {
     previewText.textContent = previewStr;
 }
 
-function displayError(message) {
-    errorText.hidden = false;
-    errorText.innerText = "ERROR: " + message;
+function clearPreview() {
+    previewText.textContent = "";
 }
 
-function loadSetPage() {
+// 0 = Sets
+// 1 = Study
+// 2 = About
+function switchSection(pageIndex) {
+    errorText.hidden = true;
+    errorText.innerText = "";
+    for (var i = 0; i < navbar.children.length; ++i) {
+        var child = navbar.children[i];
+        if (i == pageIndex) {
+            child.classList.add("active");
+            continue;
+        }
+        child.classList.remove("active");
+    }
+    loadPage(pageIndex);
+}
 
-    sessionStorage.setItem('setName',setName);
-    sessionStorage.setItem('setData',JSON.stringify(setData));
-    document.URL ='set.html'
+function loadPage(pageIndex) {
+    if (pageIndex == 0) {
+        divSetPreview.hidden = false;
+        divSetSelection.hidden = false;
+        divStudyFlashcard.hidden = true;
+        divAbout.hidden = true;
+        header.innerText = "Sets";
+        title.innerText = "Flashcards - Browsing Sets";
+    }
+    if (pageIndex == 1) {
+        divSetPreview.hidden = true;
+        divSetSelection.hidden = true;
+        divStudyFlashcard.hidden = false;
+        divAbout.hidden = true;
+        title.innerText = "Flashcards - Studying " + (setName != null) ? setName : "a set";
+        loadSetPage(validSet);
+    }
+    if (pageIndex == 2) {
+        divSetPreview.hidden = true;
+        divSetSelection.hidden = true;
+        divStudyFlashcard.hidden = true;
+        divAbout.hidden = false;
+        header.innerText = "About";
+        title.innerText = "Flashcards - About ";
+    }
+}
+
+function loadSetPage(setIsValid) {
+    if (!setIsValid) {
+        header.innerText = "Please select a valid set";
+        return;
+    }
+
+    showFlashcard();
+}
+
+function showFlashcard() {
+    studyFlashcardCounter.innerText = "Flashcard #: " + setSize;
+    studyIndexDisplay.innerText = (currentCardIndex + 1) + "/" + setSize;
+
+    if (currentCardIndex == 0) {
+        studyButtonLeft.hidden = true;
+        studyButtonRight.hidden = false;
+    } else if (currentCardIndex == setSize - 1) {
+        studyButtonLeft.hidden = false;
+        studyButtonRight.hidden = true;
+    } else {
+        studyButtonLeft.hidden = false;
+        studyButtonRight.hidden = false;
+    }
+
+    if (isTermShown) {
+        studyFlashcard.innerText = setData[currentCardIndex]['term'];
+        studyTermDisplay.innerText = "Term:";
+        return;
+    }
+    studyFlashcard.innerText = setData[currentCardIndex]['description'];
+    studyTermDisplay.innerText = "Description:";
+}
+
+function switchFlashcardLeft() {
+    if (currentCardIndex <= 0) {
+        currentCardIndex = 0;
+        return;
+    }
+    currentCardIndex--;
+    isTermShown = isTermShownDefault;
+    showFlashcard();
+}
+
+function switchFlashcardRight() {
+    if (currentCardIndex >= setSize - 1) {
+        currentCardIndex = setSize - 1;
+        return;
+    }
+    currentCardIndex++;
+    isTermShown = isTermShownDefault;
+    showFlashcard();
+}
+
+function shuffleCards() {
+
+}
+
+function flipCard() {
+    isTermShown = !isTermShown;
+    showFlashcard();
+}
+
+function clearError() {
+    divError.hidden = true;
+    errorText.innerText = "";
+}
+
+function displayError(message) {
+    divError.hidden = false;
+    errorText.innerText = "ERROR: " + message;
 }
